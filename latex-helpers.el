19bc3bc34b452b517291d0048f348dc23f92b749
joe-busch <57604085+joe-busch@users.noreply.github.com>
2021-01-15 11:24:22 -0800

    added optional argument to insert-beamer-frame

--- latex-helpers.el
+++ latex-helpers.el
@@ -1,41 +1,53 @@
-(defun insert-beamer-frame ()
+(setq lexical-binding t)
+
+(defun insert-beamer-frame (&optional align-top)
   "Creates a new frame in a LaTex beamer file"
-  (interactive)
+  (interactive "P")
+  (unless align-top
+    (message "Add any prefix argument to align top."))
   (beginning-of-line)
-  (insert "\\begin{frame}\n\\frametitle{}\n\n\\end{frame}")
-  (previous-line 2)
-  (forward-char))
+  (insert "\\begin{frame}")
+  (when align-top
+    (insert "[t]"))
+  (insert "\n\\frametitle{}\n\n\\end{frame}")
+  (forward-line -2)
+  (end-of-line)
+  (backward-char))
+
+(global-set-key (kbd "C-c f") 'insert-beamer-frame)
+
 
 (defun number-of-items ()
   "Calculates the total number of \\item's in a document."
   (interactive)
-  (defun count-items (acc) ;; tail recursively accumulate total \item's
+  (defun count-items (acc)
     (if (re-search-forward "\\\\item" nil t)
 	(count-items (1+ acc))
       acc))
   (save-excursion
     (goto-char (point-min))
-    (let ((i (count-items 0)))
+    (let ((num-items (count-items 0)))
       (cond
-       ((= i 0) (message "No items."))
-       ((= i 1) (message "1 item."))
-       (t (message "%s \\item's." i))))))
+       ((= num-items 0) (message "No \\item's."))
+       ((= num-items 1) (message "1 \\item."))
+       (t (message "%d \\item's." num-items))))))
 	
+(global-set-key (kbd "C-c i") 'number-of-items)
+
+
 (defun total-points ()
   "Calculates the total number of points in an assessment."
   (interactive)
-  (defun count-points (acc) ;; tail recursively accumulate total points
+  (defun count-points (acc)
     (if (re-search-forward "\\([[:digit:]]+\\) points*)" nil t)
 	(count-points (+ acc (string-to-int (match-string 1))))
       acc))
   (save-excursion 
     (goto-char (point-min))
-    (let ((p (count-points 0)))
+    (let ((num-points (count-points 0)))
       (cond
-       ((= p 0) (message "No points."))
-       ((= p 1) (message "1 point."))
-       (t (message "%s points." p))))))
+       ((= num-points 0) (message "No points."))
+       ((= num-points 1) (message "1 point."))
+       (t (message "%d points." num-points))))))
 
-(global-set-key (kbd "C-c f") 'insert-beamer-frame)
-(global-set-key (kbd "C-c i") 'number-of-items)
 (global-set-key (kbd "C-c p") 'total-points)
